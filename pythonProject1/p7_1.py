fdf4a0d4306835ce398f86fd586a5aa503939910
Lizikkkk <lizalen78@gmail.com>
2025-02-16 10:10:08 +0200

    p7, iter, decor

---
+++ pythonProject1/p7_1.py
@@ -0,0 +1,9 @@
+my_list = [1, 5, 2, 6, 3]
+iterator = iter(my_list)
+print(next(iterator))
+print("ВЕЛИЧЕЗНА КУПА КОДУ")
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
---
+++ pythonProject1/p7_2.py
@@ -0,0 +1,8 @@
+my_list = [1, 5, 2, 6, 3]
+iterator = iter(my_list)
+
+for elem in iterator:
+    print(elem)
+for elem in iterator:
+    print(elem)
+    
